4b0d7c897e2ca93d4903675092a78c12364b34d2
Engdaworkkassaye <engdaworkkassaye6@gmail.com>
2024-03-31 22:11:40 +0300

    created basic page setup

---
+++ react-portfolio/src/components/AboutMe.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import viteLogo from '/vite.svg'
+
+const AboutMe = () => {
+  return (
+    <section>
+      <h2>About Me</h2>
+      <div>
+      <img src={viteLogo} className="logo" alt="Vite logo" />
+        <p>Engdawork Kassaye.</p>
+      </div>
+    </section>
+  );
+};
+
+export default AboutMe;
