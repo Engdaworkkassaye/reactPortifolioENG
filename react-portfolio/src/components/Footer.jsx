cb9ca59193c31bed712539c3dfb8f5ef4250e85a
Engdaworkkassaye <engdaworkkassaye6@gmail.com>
2024-04-01 00:06:23 +0300

    added footer fixed bottom

--- react-portfolio/src/components/Footer.jsx
+++ react-portfolio/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-secondary py-3">
+    <footer className="bg-secondary py-3 fixed-bottom">
         <p className="text-light">Connect with me:</p>
     </footer>
   );
