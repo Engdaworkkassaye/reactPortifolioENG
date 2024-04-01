19b35f5eea5212ec2e72c2339520669cde26a602
Engdaworkkassaye <engdaworkkassaye6@gmail.com>
2024-04-01 16:37:06 +0300

    moved project to parent dir

---
+++ src/components/About.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from '../assets/logo.jpg'; 
+
+const About = () => {
+  return (
+    <section className="about-me">
+        <div className="row">
+          <div className="col-md-6">
+            <img src={logo} className="about-me-logo" alt="Logo" />
+          </div>
+          <div className="col-md-6">
+            <h2>About Me</h2>
+            <p>
+              Hi, I'm Engdawork Kasaye, a passionate full-stack web development student. I specialize in building responsive and user-friendly web applications using modern technologies like React, Node.js, and Express.
+            </p>
+            <p>
+              My journey in web development started 1 years ago when I discovered my passion for coding. Since then, I have been honing my skills and exploring new technologies to stay updated with the latest trends in the industry.
+            </p>
+            <p>
+              Apart from coding, I love to contribute to open-source projects, attend tech meetups, and share my knowledge with the community. In my free time, you can find me experimenting with new technologies, playing video games, or exploring the great outdoors.
+            </p>
+          </div>
+        </div>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/components/Header.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import "../App.css";
+
+function Header({ brandName, imageSrcPath, navItems }) {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleToggle = () => {
+    setExpanded(!expanded);
+  };
+
+  const handleSelect = () => {
+    setExpanded(false);
+  };
+
+  return (
+    <Navbar expand="md" bg="white" variant="light" className="shadow" expanded={expanded}>
+      <Container>
+        <Link className="navbar-brand" to="/" onClick={handleSelect}>
+          <img
+            src={imageSrcPath}
+            width="60"
+            height="60"
+            className="d-inline-block align-center"
+            alt=""
+          />
+          <span className="fw-bolder fs-4">{brandName}</span>
+        </Link>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" onClick={handleToggle} />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ms-auto mb-2 mb-md-1">
+            {navItems.map((item, index) => (
+              <Nav.Item key={index} onClick={handleSelect}>
+                <NavLink 
+                  className="nav-link" 
+                  to={`/${item.toLowerCase()}`} 
+                  activeClassName="active" // Add activeClassName
+                >
+                  {item}
+                </NavLink>
+              </Nav.Item>
+            ))}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Header;
